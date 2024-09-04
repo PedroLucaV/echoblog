8d3f2a79b5ded2e8ecfd4f57d59d520c90a1a632
Pedro Lucas <izielucas2016@gmail.com>
2024-09-04 09:35:45 -0300

    feat/creat post code finished

--- back-end/src/controllers/createPost.js
+++ back-end/src/controllers/createPost.js
@@ -1,7 +1,30 @@
 import Posts from '../model/posts.js'
 
+import { createSchema } from '../helpers/zodSchemas.js';
+import formatZodError from '../helpers/formatZodError.js';
+
 const createPost = async (req, res) => {
-    
+    const bodyValidation = createSchema.safeParse(req.body);
+
+    if(!bodyValidation.success){
+        return res.status(400).json({message: "Os dados recebidos no corpo da aplicação são invalidos", detalhes: formatZodError(bodyValidation.error)})
+    }
+
+    const {title, content, author, image} = bodyValidation.data;
+
+    try{
+        const newPost = await Posts.create({
+            title,
+            content,
+            author,
+            image
+        })
+        res.status(201).json({message: "Postagem Criada!", data: newPost});
+    }
+    catch (error){
+        console.error(error)
+        res.status(500).json({ message: "Erro ao criar postagem" });
+    }
 }
 
 export default createPost;
