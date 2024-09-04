b3163a02c8bf38794db38fd4df8556c78d69bbb6
Pedro Lucas <izielucas2016@gmail.com>
2024-09-04 08:38:41 -0300

    created project base

---
+++ back-end/src/config/dbconfig.js
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+    "echoblog",
+    "root",
+    "Sen@iDev77!.",
+    {
+        host: "localhost",
+        dialect: "mysql"
+    }
+)
+
+export default sequelize;
---
+++ back-end/src/server.js
@@ -0,0 +1,28 @@
+// imports
+import express from 'express';
+import dotenv from 'dotenv';
+import sequelize from './config/dbconfig.js';
+import bodyParser from 'body-parser';
+
+//dotenv
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+//body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res) => {
+    res.status(404).json("Page Not Found")
+})
+
+sequelize.sync().then(() => {
+    console.log('Database connected!');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch(error => {
+    console.error('Unable to connect to the database:', error);
+});
