4336672cee42aef0cca6d5edbd0adbf1ddb4c13d
Pedro Lucas <izielucas2016@gmail.com>
2024-09-04 08:49:16 -0300

    created posts model

---
+++ back-end/src/model/posts.js
@@ -0,0 +1,41 @@
+import sequelize from '../config/dbconfig.js'
+import { DataTypes } from 'sequelize'
+
+const Posts = sequelize.define(
+    'Posts', 
+    {
+        post_id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.TEXT('medium'),
+            allowNull: false,
+            required: true
+        },
+        content: {
+            type: DataTypes.TEXT('long'),
+            allowNull: false,
+            required: true
+        },
+        author: {
+            type: DataTypes.TEXT('tiny'),
+            allowNull: false,
+            required: true
+        },
+        image: {
+            type: DataTypes.TEXT('medium'),
+            allowNull: true
+        },
+        publishDate: {
+            type: DataTypes.NOW(),
+        }
+},
+{
+    tableName: "Posts",
+    timestamps: true,
+    updatedAt: true
+})
+
+export default Posts;
