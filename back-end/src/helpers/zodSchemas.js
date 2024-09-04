52d092c5f53b4c1f110f7eeb432c84d074aacb87
Pedro Lucas <izielucas2016@gmail.com>
2024-09-04 09:28:44 -0300

    create post schema finished

---
+++ back-end/src/helpers/zodSchemas.js
@@ -0,0 +1,7 @@
+import {z} from 'zod'
+
+export const createSchema = z.object({
+    title: z.string().max(255, {message: "O titulo deve conter no máximo 255 caracteres"}).min(5, {message: "O titulo conter pelo menos 5 caracteres"}),
+    content: z.string().max(2000, {message: "A descrição deve conter no máximo 2000 caracteres"}).min(10, {message: "A descrição deve conter pelo menos 10 caracteres"}),
+    author: z.string().min(3, {message: "O nome do autor deve conter pelo menos 3 caracteres"}).max(255, {message: "O nome do autor deve conter no máximo 2000 caracteres"})
+})
