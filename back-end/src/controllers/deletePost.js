dae4cf1f40e71ede8c25f02f0f0c5f13379d6b23
Pedro Lucas <izielucas2016@gmail.com>
2024-09-06 07:42:35 -0300

    RF05 delete route finished

--- back-end/src/controllers/deletePost.js
+++ back-end/src/controllers/deletePost.js
@@ -1,5 +1,23 @@
+import { getSchema } from "../helpers/zodSchemas.js"
+import Posts from "../model/posts.js"
+
 const deletePost = async (req, res) => {
-    res.end("a")
+    const idValidation = getSchema.safeParse(req.params.id)
+    if(!idValidation.success){
+        return res.status(400).json({message: "Os dados recebidos no corpo da aplicação são invalidos", detalhes: formatZodError(idValidation.error)})
+    }
+    const id = idValidation.data;
+    try {
+        const post = await Posts.findByPk(id);
+        if(!post){
+            return res.status(404).json({message: "Esta postagem não foi encontrada!"});
+        }
+        Posts.destroy({where: {post_id: id}});
+        res.status(204);
+    } catch (error) {
+        console.error(error)
+        res.status(400).json({ message: "Erro ao deletar a postagem" });
+    }
 }
 
 export default deletePost;
