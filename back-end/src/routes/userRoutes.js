c180761e7c7f37bc1c552306217adce36aba0685
Pedro Lucas <izielucas2016@gmail.com>
2024-09-09 09:36:58 -0300

    feat/Route and Middleware added

--- back-end/src/routes/userRoutes.js
+++ back-end/src/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import controllers from "../controllers/users/controllers.js";
-import verifyToken from '../helpers/verifyToken.js'
+import verifyToken from '../helpers/verifyToken.js';
+import verifyAdmin from '../helpers/verifyAdmin.js'
 
 const router = Router();
 
 router.post('/register', controllers.createUser);
 router.post('/login', controllers.loginController);
 router.put('/:id', verifyToken, controllers.editUser)
+router.get('/', verifyToken, verifyAdmin, controllers.getUsers);
 
 export default router;
