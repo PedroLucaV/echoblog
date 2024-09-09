febcff03b8585045a2a7f1b28000000af4fbde00
Pedro Lucas <izielucas2016@gmail.com>
2024-09-09 09:36:04 -0300

    added get all users to controller

--- back-end/src/controllers/users/controllers.js
+++ back-end/src/controllers/users/controllers.js
@@ -1,11 +1,13 @@
 import createUser from './createUser.js'
 import loginController from './loginController.js'
 import editUser from './editUser.js'
+import getUsers from './getUsers.js'
 
 const controllers = {
     createUser,
     loginController,
-    editUser
+    editUser,
+    getUsers
 }
 
 export default controllers
