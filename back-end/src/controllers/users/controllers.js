229d5221123388b126472b67ef6dd693cabe8fa4
Pedro Lucas <izielucas2016@gmail.com>
2024-09-09 09:59:13 -0300

    feat/Created change paper controller

--- back-end/src/controllers/users/controllers.js
+++ back-end/src/controllers/users/controllers.js
@@ -3,13 +3,15 @@ import loginController from './loginController.js'
 import editUser from './editUser.js'
 import getUsers from './getUsers.js'
 import deleteUser from './deleteUser.js'
+import changePaper from './changePaper.js'
 
 const controllers = {
     createUser,
     loginController,
     editUser,
     getUsers,
-    deleteUser
+    deleteUser,
+    changePaper
 }
 
 export default controllers
