16aeffb124756e0568e740586bd9e8d5c2706b4f
Pedro Lucas <izielucas2016@gmail.com>
2024-09-09 09:10:42 -0300

    added edit route to user routes

--- back-end/src/routes/userRoutes.js
+++ back-end/src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import controllers from "../controllers/users/controllers.js";
+import verifyToken from '../helpers/verifyToken.js'
 
 const router = Router();
 
 router.post('/register', controllers.createUser);
 router.post('/login', controllers.loginController);
+router.put('/:id', verifyToken, controllers.editUser)
 
 export default router;
