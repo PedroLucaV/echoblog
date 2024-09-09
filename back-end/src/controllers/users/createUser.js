30af47a3a888c1b83cc6e7ccccd83c25cbc02b04
Pedro Lucas <izielucas2016@gmail.com>
2024-09-09 08:16:45 -0300

    RF01-CreateUser controller finished

---
+++ back-end/src/controllers/users/createUser.js
@@ -0,0 +1,37 @@
+import Users from '../../model/users.js'
+
+import { createUser } from "../../helpers/zodSchemas.js";
+import bcrypt from 'bcrypt';
+import formatZodError from '../../helpers/formatZodError.js';
+
+const createUserRoute = async (req, res) => {
+    const bodyValidation = createUser.safeParse(req.body);
+
+    if(!bodyValidation.success){
+        return res.status(400).json({message: "Os dados recebidos no corpo da aplicação são invalidos", detalhes: formatZodError(bodyValidation.error)})
+    }
+
+    const {nome, email, senha, papel} = bodyValidation.data;
+
+    //password encrypt
+    const salt = await bcrypt.genSalt(12);
+    const senhaHash = await bcrypt.hash(senha, salt);
+
+    const user = {nome, email, senha: senhaHash, papel};
+
+    try {
+        const sameEmail = await Users.findOne({where: {email}});
+
+        if(sameEmail){
+            return res.status(403).json({message: "Já existe um usuario com este email"});
+        }
+
+        const userCreated = await Users.create(user)
+        res.status(201).json({message: "Usuario criado!"});
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Erro ao criar usuario" });
+    }
+}
+
+export default createUserRoute;
