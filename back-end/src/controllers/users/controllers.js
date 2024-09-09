dd8299dcffaf11ebbc398b3ceb68e6648fc63cdf
Pedro Lucas <izielucas2016@gmail.com>
2024-09-09 09:40:59 -0300

    feat/Added delete user to User Controllers

--- back-end/src/controllers/users/controllers.js
+++ back-end/src/controllers/users/controllers.js
@@ -2,12 +2,14 @@ import createUser from './createUser.js'
 import loginController from './loginController.js'
 import editUser from './editUser.js'
 import getUsers from './getUsers.js'
+import deleteUser from './deleteUser.js'
 
 const controllers = {
     createUser,
     loginController,
     editUser,
-    getUsers
+    getUsers,
+    deleteUser
 }
 
 export default controllers
