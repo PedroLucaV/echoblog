19f5bd16687018439f4b2c90d22866f7b52eed01
Pedro Lucas <izielucas2016@gmail.com>
2024-09-09 07:32:54 -0300

    created model user.js

---
+++ back-end/src/model/users.js
@@ -0,0 +1,41 @@
+import sequelize from '../config/dbconfig.js'
+import { DataTypes } from 'sequelize'
+
+const Users = sequelize.define(
+    'Users', 
+    {
+        user_id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        nome: {
+            type: DataTypes.TEXT('tiny'),
+            allowNull: false,
+            required: true
+        },
+        email: {
+            type: DataTypes.TEXT('medium'),
+            allowNull: false,
+            required: true
+        },
+        senha: {
+            type: DataTypes.TEXT('medium'),
+            allowNull: false,
+            required: true
+        },
+        papel: {
+            type: DataTypes.ENUM,
+            values: ["administrador", "autor", "leitor"],
+            defaultValue: "leitor",
+            allowNull: true
+        }
+},
+{
+    tableName: "Users",
+    timestamps: true,
+    createdAt: true,
+    updatedAt: true
+})
+
+export default Users;
