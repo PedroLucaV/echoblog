bebc2fe9a8a6867ed704e3e17b48a847d3835601
Pedro Lucas <izielucas2016@gmail.com>
2024-09-09 09:36:44 -0300

    fix/fixed enum in zod

--- back-end/src/helpers/zodSchemas.js
+++ back-end/src/helpers/zodSchemas.js
@@ -13,7 +13,7 @@ export const createUser = z.object({
     nome: z.string().max(255, {message: "O titulo deve conter no máximo 255 caracteres"}).min(5, {message: "O nome conter pelo menos 5 caracteres"}),
     email: z.string().max(255, {message: "O email deve conter no máximo 255 caracteres"}).min(5, {message: "O email conter pelo menos 5 caracteres"}).regex(emailRegex),
     senha: z.string().max(100, {message: "A senha deve conter no máximo 255 caracteres"}).min(8, {message: "A senha conter pelo menos 8 caracteres"}).regex(passwordRegex, {message: "A senha deve conter ao menos 1 letra maiuscula, 1 letra minuscula, 1 número e 1 Caracter Especial"}),
-    papel: z.optional(z.enum([["administrador", "autor", "leitor"]]))
+    papel: z.optional(z.enum(["administrador", "autor", "leitor"]))
 })
 
 export const updateUser = z.object({
