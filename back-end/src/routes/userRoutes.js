d9851613bee10e7d2922b2076eed8ebf093ff6b6
Pedro Lucas <izielucas2016@gmail.com>
2024-09-09 08:23:27 -0300

    login route created

--- back-end/src/routes/userRoutes.js
+++ back-end/src/routes/userRoutes.js
@@ -4,5 +4,6 @@ import controllers from "../controllers/users/controllers.js";
 const router = Router();
 
 router.post('/register', controllers.createUser);
+router.post('/login', controllers.loginController);
 
 export default router;
