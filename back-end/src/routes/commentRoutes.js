1637461fd819e8bcc0e3115f2b489a3227d0a7dd
Pedro Lucas <izielucas2016@gmail.com>
2024-09-18 09:57:35 -0300

    feat/added edit comment route

--- back-end/src/routes/commentRoutes.js
+++ back-end/src/routes/commentRoutes.js
@@ -1,6 +1,10 @@
 import {Router} from 'express';
 import controllers from '../controllers/comments/controllers.js'
+import verifyRole from "../helpers/verifyRole.js";
+import verifyToken from "../helpers/verifyToken.js";
 
 const router = Router();
+
+router.put('/:id', verifyToken, verifyRole, controllers.editPost)
 
 export default router;
