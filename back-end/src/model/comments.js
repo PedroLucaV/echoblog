dc3726ffc7e0e3fee96c79788b0aa01c3aedabb5
Pedro Lucas <izielucas2016@gmail.com>
2024-09-18 09:15:43 -0300

    feat/Created comment model with association

---
+++ back-end/src/model/comments.js
@@ -0,0 +1,26 @@
+import sequelize from '../config/dbconfig.js'
+import { DataTypes, UUIDV4 } from 'sequelize'
+import Posts from './posts.js';
+import Users from './users.js';
+
+const Comments = sequelize.define('Comments',
+{
+    comment_id: {
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    comment: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+{
+    tableName: 'Comments'
+})
+
+Posts.belongsToMany(Users, {through: Comments});
+Users.belongsToMany(Posts, {through: Comments});
+
+export default Comments;
